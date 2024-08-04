ea31f2a66b6fa1f837e3d5bf6e465f2a7b62de36
DuyTruong159 <truongnhox159@gmail.com>
2024-08-04 19:22:50 -0400

    done

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
     $(function() {
         $("#header").load("./header.html");
-        $("#footer").load("./footer.html");
+        $("#footer").load("./footer.html", function() {
+            $('input[type="submit"]').click(function(e) {
+                e.preventDefault();
+        
+                alert("We will contact you shortly!!");
+            });
+        });
     });
+
+    $('input[type="submit"]').click(function(e) {
+        e.preventDefault();
+
+        alert("We will contact you shortly!!");
+    });
+
+    console.log($(location).attr("href"));
+    console.log($(location).attr("origin"));
+    console.log($(location).attr("pathname"));
 });
